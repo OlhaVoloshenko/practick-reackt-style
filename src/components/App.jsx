658f1f2ed5a6b0ebd49a9affb9e10e4610382891
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-07-07 10:19:26 +0300

    11
    11

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { PageTitle } from 'components/PageTitle/PageTitle';
+import { EventBoard } from 'components/EventBoard/EventBoard';
 
 export const App = () => {
   return (
     <div>
       <PageTitle text="24th Core Worlds Coalition Conference" />
+      <EventBoard />
     </div>
   );
 };
